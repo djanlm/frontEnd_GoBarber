18a1763cb138cbc0a8622ac734eb11364314bf93
Djanilson Martins <djanlm@hotmail.com>
2019-10-11 20:53:24 -0300

    updating profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your e-mail address" />
+        <hr />
+        <Input type="password" name="oldPassword" placeholder="Your password" />
+        <Input type="password" name="password" placeholder="New password" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm new password"
+        />
+        <button type="submit">Update Profile</button>
+      </Form>
+      <button type="button">Logout</button>
+    </Container>
+  );
 }
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,23 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+    const profile = Object.assign(
+      { name, email },
+      rest.oldPassword ? rest : {}
+    ); // junta dois objetos
+
+    const response = yield call(api.put, 'users', profile);
+    yield put(updateProfileSuccess(response.data));
+    toast.success('Perfil atualizado com sucesso!');
+  } catch (err) {
+    toast.error('Error updating your profile, check your form!');
+    yield put(updateProfileFailure());
+  }
+}
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
