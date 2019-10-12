0a80999ba53e759e0fef6f0fc41690f5837ece1d
Djanilson Martins <djanlm@hotmail.com>
2019-10-12 19:23:59 -0300

    logout&upload

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import en from 'date-fns/locale/en-US';
 
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>dashboard</h1>;
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(() => format(date, 'MMMM d ', { locale: en }), [
+    date,
+  ]);
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1)); // subtrai 1 dia
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1)); // add one day
+  }
+
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={handlePrevDay}>
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={handleNextDay}>
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Djanilson Martins</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Open</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Djanilson Martins</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Djanilson Martins</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import { signOut } from '~store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
+import AvatarInput from './AvatarInput';
 import { Container } from './styles';
 
 export default function Profile() {
@@ -13,9 +15,13 @@ export default function Profile() {
     dispatch(updateProfileRequest(data));
   }
 
+  function handleSignOut() {
+    dispatch(signOut());
+  }
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Full name" />
         <Input name="email" type="email" placeholder="Your e-mail address" />
         <hr />
@@ -28,7 +34,9 @@ export default function Profile() {
         />
         <button type="submit">Update Profile</button>
       </Form>
-      <button type="button">Logout</button>
+      <button type="button" onClick={handleSignOut}>
+        Logout
+      </button>
     </Container>
   );
 }
